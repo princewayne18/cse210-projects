53225baf194d306049df00ebd045cb1cd5761f58
princewayne18 <dubeprince18@outlook.com>
2023-09-14 23:07:59 +0200

    prep 1 done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("what is Your name? ");
+        string name =Console.ReadLine();
+
+        Console.Write("what is your last surname? ");
+        string last_name= Console.ReadLine();
+
+        Console.WriteLine($"Your name is {name}, {name} {last_name}");
+
+
     }
 }
