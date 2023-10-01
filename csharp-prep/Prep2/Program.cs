00a42151d77671fcd2a98d54c2e669078f83a23d
Your Name <you@example.com>
2023-10-01 11:12:46 +0200

    new

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.Write("what is your grade percentage: ");
+        string grade = Console.ReadLine();
+        int gradeP =int.Parse(grade);
+
+        if (gradeP > 90)
+        {
+            Console.WriteLine("A");
+        }
+
+        else if (gradeP>80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (gradeP>70)
+        {
+            Console.WriteLine("C"); 
+        }
+        else if (gradeP>60)
+        {
+            Console.WriteLine("D");
+        }
+        else 
+        {
+            Console.WriteLine("F");
+        }
+
+
+    }    
 }
