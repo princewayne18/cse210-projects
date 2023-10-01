f9143bf1d5525c895c914dc5620cd4288b6ce85c
Your Name <you@example.com>
2023-10-01 13:15:48 +0200

    full project

---
+++ prepare/Learning02/resume.cs
@@ -0,0 +1,25 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Resume
+{
+   public string _name;
+
+   public List<Job> _job = new List<Job>();
+
+   public void Display()
+   {
+    Console.WriteLine($"Name: {_name}");
+    Console.WriteLine("Jobs: ");
+
+
+    foreach (Job job in _job)
+    {
+        job.Display();
+    }
+
+   }
+
+}
+
+
