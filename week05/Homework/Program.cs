7776f27dad49e59af447e8e2c3758dde39068d3f
princewayne18 <pdube1@byupathway.edu>
2026-02-19 18:07:27 +0100

    homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Prince", "History");
+        string summary = assignment.GetSummary();
+        Console.WriteLine(summary);
+        Console.WriteLine();
+
+        MathAssignment mathassignment = new MathAssignment("Nqobile", "Fractions", "7.3", "8.9");
+        string a1 = mathassignment.GetHomeworkList();
+        string summary2 = assignment.GetSummary();
+
+        Console.WriteLine(a1);
+        Console.Write(summary2);
+        Console.WriteLine();
+
+        WritingAssignment wa = new WritingAssignment("Mary Waters", "- European History", "The Causes of World War II");
+        string a2 = wa.GetWritingInformation();
+
+        Console.Write(a2);
+
     }
 }
